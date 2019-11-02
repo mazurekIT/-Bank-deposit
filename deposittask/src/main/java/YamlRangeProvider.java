275e418bb6b9469084845377dba5e94cd22aa496
Mazzi <mazzi198913@wp.pl>
2019-11-03 00:56:13 +0100

    solved problem with parsing yaml file

--- deposittask/src/main/java/YamlRangeProvider.java
+++ deposittask/src/main/java/YamlRangeProvider.java
@@ -1,10 +1,16 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class YamlRangeProvider implements RangeProvider {
     private String filePath;
@@ -20,14 +26,33 @@ public class YamlRangeProvider implements RangeProvider {
     @Override
     public List<Range> getAvailableRanges() {
         List<Range> obj = new ArrayList<>();
-        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-        objectMapper.findAndRegisterModules();
-        try {
-            RangeList rangeList = objectMapper.readValue(new File(filePath),RangeList.class);
-        } catch (IOException e) {
-            System.out.println("eeeeeeeeeeeeeeeeeee");
-        }
+        Yaml yaml = new Yaml();
+        try (FileInputStream fileInputStream = new FileInputStream(new File(filePath))) {
+            LinkedHashMap<String,ArrayList<LinkedHashMap<String,Number>>> load = yaml.load(fileInputStream);
+            ArrayList<LinkedHashMap<String,Number>> rangeList = load.get("ranges");
+            for (LinkedHashMap<String,Number> r:rangeList){
+                Number rangeFrom = r.get(UNIT_NAME_FOR_RANGE_FROM);
+                Number rangeTo = r.get(UNIT_NAME_FOR_RANGE_TO);
+                Number interest = r.get(UNIT_NAME_FOR_INTEREST);
+                obj.add(new Range(
+                        convertDoubleToBigDecimal(rangeFrom),
+                        convertDoubleToBigDecimal(rangeTo),
+                        convertDoubleToBigDecimal(interest)
+                ));
+            }
 
-        return obj;
+            return obj;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+    private BigDecimal convertDoubleToBigDecimal(Number number){
+        return new BigDecimal(number.doubleValue());
     }
 }
