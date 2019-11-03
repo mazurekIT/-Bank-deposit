7a673263c6f15674e7c4687f19b928f7c7661642
Mazzi <mazzi198913@wp.pl>
2019-11-03 01:02:10 +0100

    solved problem with parsing yaml file

--- deposittask/src/main/java/YamlRangeProvider.java
+++ deposittask/src/main/java/YamlRangeProvider.java
@@ -1,5 +1,3 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
@@ -10,7 +8,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class YamlRangeProvider implements RangeProvider {
     private String filePath;
@@ -28,22 +25,19 @@ public class YamlRangeProvider implements RangeProvider {
         List<Range> obj = new ArrayList<>();
         Yaml yaml = new Yaml();
         try (FileInputStream fileInputStream = new FileInputStream(new File(filePath))) {
-            LinkedHashMap<String,ArrayList<LinkedHashMap<String,Number>>> load = yaml.load(fileInputStream);
-            ArrayList<LinkedHashMap<String,Number>> rangeList = load.get("ranges");
-            for (LinkedHashMap<String,Number> r:rangeList){
-                Number rangeFrom = r.get(UNIT_NAME_FOR_RANGE_FROM);
-                Number rangeTo = r.get(UNIT_NAME_FOR_RANGE_TO);
-                Number interest = r.get(UNIT_NAME_FOR_INTEREST);
+            LinkedHashMap<String, ArrayList<LinkedHashMap<String, Number>>> load = yaml.load(fileInputStream);
+            ArrayList<LinkedHashMap<String, Number>> rangeList = load.get("ranges");
+            for (LinkedHashMap<String, Number> r : rangeList) {
                 obj.add(new Range(
-                        convertDoubleToBigDecimal(rangeFrom),
-                        convertDoubleToBigDecimal(rangeTo),
-                        convertDoubleToBigDecimal(interest)
+                        convertDoubleToBigDecimal(r.get(UNIT_NAME_FOR_RANGE_FROM)),
+                        convertDoubleToBigDecimal(r.get(UNIT_NAME_FOR_RANGE_TO)),
+                        convertDoubleToBigDecimal(r.get(UNIT_NAME_FOR_INTEREST))
                 ));
             }
 
             return obj;
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            throw new RangesReadException("Błędna ścieżka pliku YAML",e);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -51,8 +45,7 @@ public class YamlRangeProvider implements RangeProvider {
     }
 
 
-
-    private BigDecimal convertDoubleToBigDecimal(Number number){
+    private BigDecimal convertDoubleToBigDecimal(Number number) {
         return new BigDecimal(number.doubleValue());
     }
 }
